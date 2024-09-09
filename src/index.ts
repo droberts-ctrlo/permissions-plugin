5436f56c872d8bb378087bf4911bf036298aa9f1
Dave Roberts <d.roberts@ctrlo.com>
2024-09-09 11:27:10 +0100

    Updated to TS

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { exec } from 'child_process';
+import { Compiler } from 'webpack';
+
+class RestorePlugin {
+    handleMessage(err: any, stdout: any, stderr: any) {
+        if (err) console.error(err);
+        if (stdout) console.log(stdout);
+        if (stderr) console.error(stderr);
+    }
+
+    apply(compiler: Compiler) {
+        compiler.hooks.done.tap('PermissionsPlugin', () => {
+            exec('chmod -Rc o+rX .', this.handleMessage);
+        });
+    }
+}
+
+module.exports = RestorePlugin;
